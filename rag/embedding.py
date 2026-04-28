3cd08d46e89373de56b62159e1590038c77cafca
ThomasVS98 <r0655289@student.thomasmore.be>
2026-04-28 11:38:29 +0200

    als gpu aanwezig is, zal embedding fase hiermee gebeuren

--- rag/embedding.py
+++ rag/embedding.py
@@ -1,12 +1,21 @@
+import torch
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from utils.logging import get_logger
+
+logger = get_logger(__name__)
 
 _embed_model = None
 
 def get_embed_model():
     global _embed_model
     if _embed_model is None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        logger.info(f"Embedding draait op: {device}")
+
         _embed_model = HuggingFaceEmbedding(
             model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2",
-            normalize=True
+            normalize=True,
+            device=device
         )
     return _embed_model
